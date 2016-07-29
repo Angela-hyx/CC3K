5281e6ec690f31145cf48d2836536591dd1a2061
Angela-hyx <hyxangela@gmail.com>
2016-07-29 14:14:36 -0400

    Add files via upload

---
+++ gold.h
@@ -0,0 +1,24 @@
+#ifndef _GOLD_H_
+#define _GOLD_H_
+#include "../item.h"
+
+#include <string>
+
+class Gold: public Item {
+   int quantity;
+   bool pickable;
+  public:
+   Gold(int row, int col, std::string type, char symbol, int quantity, bool pickable);
+    ~Gold();
+    //Gold(Gold&&) = default;
+    //Gold& operator=(Gold&&) = default;
+   int getQuantity();
+   bool isPickable();
+   void setQuantity(int num);
+   void setPickable(bool pick);
+};
+
+#endif
+
+
+
